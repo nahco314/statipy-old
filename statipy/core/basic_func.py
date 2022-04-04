f70f72a775ff3ed914bbc806a8a791d00804a0c6
nahco314 <nahco3_ta@yahoo.co.jp>
2022-04-05 02:58:55 +0900

    エディタの型チェッカのためにアノテーションを追加

--- statipy/core/basic_func.py
+++ statipy/core/basic_func.py
@@ -1,6 +1,7 @@
 from statipy.core.abstract_object import AbstractObject, py_not_implemented
 import statipy.errors as errors
 from statipy.core.environment import Environment
+from typing import TypeAlias, Callable
 
 
 # ここらへんの関数群はAbstractObjectのメソッドにしたほうが良い気がする
@@ -41,6 +42,11 @@ def UNARY_FUNC(method_name: str):
         raise errors.TypeError()
 
 
+binary_func: TypeAlias = Callable[[Environment, AbstractObject, AbstractObject], AbstractObject]
+binary_i_func: TypeAlias = Callable[[Environment, AbstractObject, AbstractObject], AbstractObject]
+unary_func: TypeAlias = Callable[[Environment, AbstractObject], AbstractObject]
+
+
 def index_check(obj: AbstractObject) -> bool:
     return getattr(obj.type, "index", None) is not None  # hasattr?
 
@@ -76,36 +82,36 @@ def binary_i_op1(env: Environment, a: AbstractObject, b: AbstractObject, i_op: s
     return res
 
 
-py_add = BINARY_FUNC("add")  # sq.concat?
-py_sub = BINARY_FUNC("sub")
-py_mul = BINARY_FUNC("mul")  # sq.repeat?
-py_div = BINARY_FUNC("div")
-py_floordiv = BINARY_FUNC("floordiv")
-py_mod = BINARY_FUNC("mod")
-py_pow = BINARY_FUNC("pow")
-py_lshift = BINARY_FUNC("lshift")
-py_rshift = BINARY_FUNC("rshift")
-py_or = BINARY_FUNC("or")
-py_xor = BINARY_FUNC("xor")
-py_and = BINARY_FUNC("and")
-py_matmul = BINARY_FUNC("matmul")
+py_add: binary_func = BINARY_FUNC("add")  # sq.concat?
+py_sub: binary_func = BINARY_FUNC("sub")
+py_mul: binary_func = BINARY_FUNC("mul")  # sq.repeat?
+py_div: binary_func = BINARY_FUNC("div")
+py_floordiv: binary_func = BINARY_FUNC("floordiv")
+py_mod: binary_func = BINARY_FUNC("mod")
+py_pow: binary_func = BINARY_FUNC("pow")
+py_lshift: binary_func = BINARY_FUNC("lshift")
+py_rshift: binary_func = BINARY_FUNC("rshift")
+py_or: binary_func = BINARY_FUNC("or")
+py_xor: binary_func = BINARY_FUNC("xor")
+py_and: binary_func = BINARY_FUNC("and")
+py_matmul: binary_func = BINARY_FUNC("matmul")
 
 
-py_inplace_add = INPLACE_BINARY_FUNC("add")  # sq.concat?
-py_inplace_sub = INPLACE_BINARY_FUNC("sub")
-py_inplace_mul = INPLACE_BINARY_FUNC("mul")  # sq.repeat?
-py_inplace_div = INPLACE_BINARY_FUNC("div")
-py_inplace_floordiv = INPLACE_BINARY_FUNC("floordiv")
-py_inplace_mod = INPLACE_BINARY_FUNC("mod")
-py_inplace_pow = INPLACE_BINARY_FUNC("pow")
-py_inplace_lshift = INPLACE_BINARY_FUNC("lshift")
-py_inplace_rshift = INPLACE_BINARY_FUNC("rshift")
-py_inplace_or = INPLACE_BINARY_FUNC("or")
-py_inplace_xor = INPLACE_BINARY_FUNC("xor")
-py_inplace_and = INPLACE_BINARY_FUNC("and")
-py_inplace_matmul = INPLACE_BINARY_FUNC("matmul")
+py_inplace_add: binary_i_func = INPLACE_BINARY_FUNC("add")  # sq.concat?
+py_inplace_sub: binary_i_func = INPLACE_BINARY_FUNC("sub")
+py_inplace_mul: binary_i_func = INPLACE_BINARY_FUNC("mul")  # sq.repeat?
+py_inplace_div: binary_i_func = INPLACE_BINARY_FUNC("div")
+py_inplace_floordiv: binary_i_func = INPLACE_BINARY_FUNC("floordiv")
+py_inplace_mod: binary_i_func = INPLACE_BINARY_FUNC("mod")
+py_inplace_pow: binary_i_func = INPLACE_BINARY_FUNC("pow")
+py_inplace_lshift: binary_i_func = INPLACE_BINARY_FUNC("lshift")
+py_inplace_rshift: binary_i_func = INPLACE_BINARY_FUNC("rshift")
+py_inplace_or: binary_i_func = INPLACE_BINARY_FUNC("or")
+py_inplace_xor: binary_i_func = INPLACE_BINARY_FUNC("xor")
+py_inplace_and: binary_i_func = INPLACE_BINARY_FUNC("and")
+py_inplace_matmul: binary_i_func = INPLACE_BINARY_FUNC("matmul")
 
 
-py_negative = UNARY_FUNC("negative")
-py_positive = UNARY_FUNC("positive")
-py_invert = UNARY_FUNC("invert")
+py_negative: unary_func = UNARY_FUNC("negative")
+py_positive: unary_func = UNARY_FUNC("positive")
+py_invert: unary_func = UNARY_FUNC("invert")

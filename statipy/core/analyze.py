0ed255e252c33155b9fcbc8a73197edcc0df1ff6
nahco314 <nahco3_ta@yahoo.co.jp>
2022-04-18 17:44:26 +0900

    envを同時に取る関数を追加

--- statipy/core/analyze.py
+++ statipy/core/analyze.py
@@ -1,11 +1,23 @@
 from statipy.core.typer import Typer
+import statipy.core.typed_ast as t_ast
+from statipy.core.environment import Environment
+
 import ast
 
 
-def analyze(code: str):
+def analyze(code: str) -> t_ast.Typedmod:
     """
-    Analyze the code and return a dict with the results.
+    Analyze the code and return a result in AST.
     """
     typer = Typer(code)
     result = typer.analyze()
     return result
+
+
+def analyze_env(code: str) -> tuple[t_ast.Typedmod, Environment]:
+    """
+    Analyze the code and return AST and Environment with variable information.
+    """
+    typer = Typer(code)
+    result = typer.analyze()
+    return result, typer.env

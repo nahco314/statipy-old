08027dc5fab8edc7bd5a753dc8457be15f7c7957
nahco314 <nahco3_ta@yahoo.co.jp>
2022-05-16 20:06:59 +0900

    typerのクラスをほかのに差し替えられるようにした

--- statipy/core/analyze.py
+++ statipy/core/analyze.py
@@ -1,23 +1,25 @@
-from statipy.core.typer import Typer
+from statipy.core.typer import Typer, TyperBase
 import statipy.core.typed_ast as t_ast
 from statipy.core.environment import Environment
+
+from typing import Type
 
 import ast
 
 
-def analyze(code: str) -> t_ast.Typedmod:
+def analyze(code: str, typer_cls: Type[TyperBase] = Typer) -> t_ast.Typedmod:
     """
     Analyze the code and return a result in AST.
     """
-    typer = Typer(code)
+    typer = typer_cls(code)
     result = typer.analyze()
     return result
 
 
-def analyze_env(code: str) -> tuple[t_ast.Typedmod, Environment]:
+def analyze_env(code: str, typer_cls: Type[TyperBase] = Typer) -> tuple[t_ast.Typedmod, Environment]:
     """
     Analyze the code and return AST and Environment with variable information.
     """
-    typer = Typer(code)
+    typer = typer_cls(code)
     result = typer.analyze()
     return result, typer.env
